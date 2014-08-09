695f415725e01c2c9b50abd96ebf54d455f53053
Johannes Ekberg <uppfinnarn@gmail.com>
2014-08-09 11:35:54 +0200

    Now it actually compiles too

--- include/kclib/LKNetworkImpl.h
+++ include/kclib/LKNetworkImpl.h
@@ -2,10 +2,15 @@
 #define KCLIB_LKNETWORKIMPL_H
 
 #include <string>
+#include <vector>
+#include <functional>
 
 class LKNetworkImpl
 {
 public:
+	LKNetworkImpl() {};
+	virtual ~LKNetworkImpl() {};
+	
 	virtual void get(const std::string &url, std::function<void(bool success, std::vector<char> body)> callback) = 0;
 	virtual void post(const std::string &url, std::vector<char> body, std::function<void(bool success, std::vector<char> body)> callback) = 0;
 };
