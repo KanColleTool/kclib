aeda435c8c446ed75b5149223e2ab0d85a13d1b2
Johannes Ekberg <uppfinnarn@gmail.com>
2014-11-08 23:41:48 +0100

    Use lowercase hex representations

--- tests/test_LKUtil_escaping.cpp
+++ tests/test_LKUtil_escaping.cpp
@@ -5,16 +5,16 @@
 
 TEST_CASE("Can unescape Naka-chan's name")
 {
-	std::string s1("\u90A3\u73C2");
-	std::string s2 = unescape("\\u90A3\\u73C2");
+	std::string s1("\u90a3\u73c2");
+	std::string s2 = unescape("\\u90a3\\u73c2");
 	
 	REQUIRE(s1 == s2);
 }
 
 TEST_CASE("Can unescape a sentence with Naka-chan's name")
 {
-	std::string s1("...\u90A3\u73C2-chan dayo~");
-	std::string s2 = unescape("...\\u90A3\\u73C2-chan dayo~");
+	std::string s1("...\u90a3\u73c2-chan dayo~");
+	std::string s2 = unescape("...\\u90a3\\u73c2-chan dayo~");
 	
 	REQUIRE(s1 == s2);
 }
